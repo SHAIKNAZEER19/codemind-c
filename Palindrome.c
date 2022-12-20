9b2a1cd0c5516383780fad845249bed8e4b83438
shaik nazeer <119719049+SHAIKNAZEER19@users.noreply.github.com>
2022-12-20 23:02:41 +0530

    #include<stdio.h> int main() { int n,r,sum=0,temp; scanf("%d",&n); temp=n; while(n>0) { r=n%10; sum=(sum*10)+r; n=n/10; } if(temp==sum) printf("True"); else printf("False"); return 0; }

--- Palindrome.c
+++ Palindrome.c
@@ -1,34 +1,18 @@
-#include<stdio.h>
-int is_pal(int);
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    if(is_pal(n))
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
-}
-int is_pal(int m)
-{
-    int q,r,s=0;
-    q=m;
-    while(q!=0)
-    {
-        r=q%10;
-        s=s*10+r;
-        q=q/10;
-    }
-    if(s==m)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;  
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("True");    
+else    
+printf("False");   
+return 0;  
 }
